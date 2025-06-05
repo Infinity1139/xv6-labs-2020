2c40da4301ed986096f3f659c7f9ddb7054e54f4
infinity <infinity@xv6.myguest.virtualbox.org>
2025-06-05 16:39:15 +0800

    finish mmap

--- kernel/proc.c
+++ kernel/proc.c
@@ -109,6 +109,12 @@ allocproc(void)
       release(&p->lock);
     }
   }
+
+  // Clear VMAs
+  for(int i=0;i<NVMA;i++) {
+    p->vmas[i].valid = 0;
+  }
+
   return 0;
 
 found:
@@ -146,6 +152,10 @@ freeproc(struct proc *p)
   if(p->trapframe)
     kfree((void*)p->trapframe);
   p->trapframe = 0;
+  for(int i = 0; i < NVMA; i++) {
+    struct vma *v = &p->vmas[i];
+    vmaunmap(p->pagetable, v->vastart, v->sz, v);
+  }
   if(p->pagetable)
     proc_freepagetable(p->pagetable, p->sz);
   p->pagetable = 0;
@@ -296,6 +306,16 @@ fork(void)
       np->ofile[i] = filedup(p->ofile[i]);
   np->cwd = idup(p->cwd);
 
+   // copy vmas created by mmap.
+  // actual memory page as well as pte will not be copied over.
+  for(i = 0; i < NVMA; i++) {
+    struct vma *v = &p->vmas[i];
+    if(v->valid) {
+      np->vmas[i] = *v;
+      filedup(v->f);
+    }
+  }
+  
   safestrcpy(np->name, p->name, sizeof(p->name));
 
   pid = np->pid;
